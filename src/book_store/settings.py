e7a530aebce6a290cfd55478e5f18a0dd7e9f505
Arezoo Moradi <arezoomoradichegni@eng.ac.ir>
2021-08-11 22:47:20 +0430

    second commit

--- src/book_store/settings.py
+++ src/book_store/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #my app
+    # app
     'account',
     'books',
     'customers',
