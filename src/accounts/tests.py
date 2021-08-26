e6a35fc5b81d1b9d81aeac54839904b5c3680adb
Arezoo Moradi <arezoomoradichegni@eng.ac.ir>
2021-08-26 22:47:34 +0430

    five commit

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Address
+
+
+# Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='john',
+            email='john@gmail.com',
+            password='jj123456'
+        )
+        self.assertEqual(user.username, 'john')
+        self.assertEqual(user.email, 'john@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class AddressTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@gmail.com',
+            password='secret'
+        )
+        self.address = Address.objects.create(
+            province='A good title',
+            city='Nice body content',
+            full_address='just for test',
+            postal_code='123456',
+            user=self.user
+        )
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.address.province}', 'A good title')
+        self.assertEqual(f'{self.address.user}', 'test@gmail.com')
+        self.assertEqual(f'{self.address.city}', 'Nice body content')
--- src/product/tests.py
+++ src/product/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        Category.objects.create(name='داستان')
+
+        def test_text_content(self):
+            cat = Category.objects.get(id=1)
+            expected_object_name = f'{cat.name}'
+            self.assertEqual(expected_object_name, 'داستان')
